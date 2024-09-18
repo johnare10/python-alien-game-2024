a2a59173c80a607a5d76bc5a89df53079be31f5f
John <john.arevalo96@gmail.com>
2024-09-17 21:17:55 -0400

    add settings, ship, and update main game code with; refractor

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,6 @@
 import sys
+from settings import Settings
+from ship import Ship
 
 import pygame
 
@@ -7,21 +9,36 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game and create the game resources"""
         pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+    
 
-
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        
+        # Create the ship
+        self.ship = Ship(self)
+
 
     def run_game(self):
         """Start main loop for game"""
         while True:
-            # Watch for keyboard and mouse events
-            for event in pygame.event.get():
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+        """Responds to keypress etc"""
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit
+    
+    def _update_screen(self):
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+         
 
 if __name__ == '__main__':
     # Make a  game instance, and run the game.
